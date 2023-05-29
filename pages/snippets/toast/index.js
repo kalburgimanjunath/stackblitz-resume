a9a9f3790109f866d24f095d46cba09161197cd7
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-29 21:39:09 +0530

    2 files modified

--- pages/snippets/toast/index.js
+++ pages/snippets/toast/index.js
@@ -1,15 +1,20 @@
+import { CodeSection } from 'react-code-section-lib';
 export default function Index() {
+  const value = ` import { render, screen } from '@testing-library/react';
+ import App from './App';
+  
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });`;
   return (
     <div>
       <h3>Toast</h3>
       <div>A simple reusable notification toasts</div>
-      <pre>
-        <code language="language-html">
-          <html>
-            <body></body>
-          </html>
-        </code>
-      </pre>
+      <CodeSection showLineNumbers theme="dark" lang="react">
+        {value}
+      </CodeSection>
     </div>
   );
 }
