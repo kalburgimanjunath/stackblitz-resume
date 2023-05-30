abc638d0377bbbebcde7cf5e1bace3245b7afee7
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-30 08:50:07 +0530

    2 files modified, 1 added

--- pages/snippets/toast/index.js
+++ pages/snippets/toast/index.js
@@ -1,6 +1,7 @@
 import { CodeSection } from 'react-code-section-lib';
+import Link from 'next/link';
 export default function Index() {
-  const value = `import { ToastContainer } from "react-toastify";
+  const example = `import { ToastContainer } from "react-toastify";
   import "react-toastify/dist/ReactToastify.css";
   
   function MyApp({ Component, pageProps }) {
@@ -21,13 +22,48 @@ export default function Index() {
     router.push("/login");
   };
   `;
+  const usage = `npm i react-toastify
+  `;
+  const imports = `import { ToastContainer } from "react-toastify";
+  import "react-toastify/dist/ReactToastify.css";`;
+  const syntax = `toast.success("🚀 Logged out successfully!");
+`;
   return (
     <div>
       <h3>Toast</h3>
       <div>A simple reusable notification toasts</div>
-      <CodeSection showLineNumbers theme="dark" lang="react">
-        {value}
+      <CodeSection theme="dark" lang="react">
+        {example}
       </CodeSection>
+      <h3>Usage</h3>
+      <ol>
+        <li>
+          <h4>Install react-toastify npm package</h4>
+          <CodeSection showLineNumbers theme="dark" lang="react">
+            {usage}
+          </CodeSection>
+        </li>
+        <li>
+          <h4>Import in the root directory</h4>
+          <CodeSection theme="dark" lang="react">
+            {imports}
+          </CodeSection>
+        </li>
+        <li>
+          <h4>Use toast in your app</h4>
+          <CodeSection theme="dark" lang="react">
+            {syntax}
+          </CodeSection>
+        </li>
+      </ol>
+      <>
+        <Link href="../../../demos/toast/" target="_new">
+          Live Demo
+        </Link>
+        <Link href="/" target="_new">
+          Source Code
+        </Link>
+      </>
     </div>
   );
 }
