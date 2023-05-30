d006607bda1dbdab9a2d88c3e7a941ba16ac04ef
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-05-30 19:57:16 +0530

    1 file modified

--- pages/quotes.js
+++ pages/quotes.js
@@ -7,6 +7,29 @@ export default function Quotes() {
   const submitData = () => {
     toast.success('🚀 Submitted successfully!');
   };
+  const saveData = (data) => {
+    const url = `https://api.airtable.com/v0/appkjlwWrVqVpSC7i/Quotes`;
+    const requestOptions = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer keyeNXyxxuuYJY19w',
+      },
+      body: JSON.stringify({
+        fields: {
+          message: textValue,
+        },
+      }),
+      typecast: true,
+    };
+    console.log(requestOptions);
+    fetch(url, requestOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+    toast.success('🚀 Submitted successfully!');
+  };
   return (
     <div className="p-24">
       <h3>Quotes</h3>
@@ -26,7 +49,7 @@ export default function Quotes() {
       <button
         type="submit"
         className="p-5 mt-10 bg-blue-100"
-        onClick={submitData}
+        onClick={saveData}
       >
         Write
       </button>
